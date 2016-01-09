6fcf9a1ce2a61c757197afa7f66a783a7c4e34b9
viniciuslrangel <vinicius.l.rangel@gmail.com>
2016-01-09 03:38:34 -0200

    Workspace setup

---
+++ src/main/java/io/github/viniciuslrangel/CreativePeripheral/CreativePeripheral.java
@@ -0,0 +1,11 @@
+package io.github.viniciuslrangel.CreativePeripheral;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CreativePeripheral.MODID, version = CreativePeripheral.VERSION)
+public class CreativePeripheral
+{
+    public static final String MODID = "CreativePeripheral";
+    public static final String VERSION = "1.0";
+
+}
