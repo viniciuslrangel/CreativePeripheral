f3f0a884c32514f633ef72e602f861dc7047694e
viniciuslrangel <vinicius.l.rangel@gmail.com>
2016-01-09 15:48:20 -0200

    Add ComputerCraft API

--- src/main/java/io/github/viniciuslrangel/CreativePeripheral/CreativePeripheral.java
+++ src/main/java/io/github/viniciuslrangel/CreativePeripheral/CreativePeripheral.java
@@ -1,11 +1,17 @@
 package io.github.viniciuslrangel.CreativePeripheral;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
-@Mod(modid = CreativePeripheral.MODID, version = CreativePeripheral.VERSION)
+@Mod(modid = CreativePeripheral.MODID, version = CreativePeripheral.VERSION, dependencies = "required-after:ComputerCraft")
 public class CreativePeripheral
 {
     public static final String MODID = "CreativePeripheral";
     public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+
+    }
 
 }
