de064eb660013199a4a47bb6242c28f518637116
viniciuslrangel <vinicius.l.rangel@gmail.com>
2016-01-10 22:09:52 -0200

    CreativeAPI lua file(not working)
    add commands:
    getWorldSeed([number dimensionId])
    getWorldTime(number dimensionId)
    isWorldBlockLoaded(number dimensionId, number x, number y, number z)
    getBlock(number dimensionId, number x, number y, number z)

--- src/main/java/io/github/viniciuslrangel/CreativePeripheral/CreativePeripheral.java
+++ src/main/java/io/github/viniciuslrangel/CreativePeripheral/CreativePeripheral.java
@@ -7,8 +7,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = CreativePeripheral.MODID, version = CreativePeripheral.VERSION, dependencies = "required-after:ComputerCraft")
-public class CreativePeripheral
-{
+public class CreativePeripheral {
     public static final String MODID = "CreativePeripheral";
     public static final String VERSION = "1.0";
 
@@ -18,13 +17,13 @@ public class CreativePeripheral
     public BlockCreativePeripheral blockCreativePeripheral;
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event){
+    public void preInit(FMLPreInitializationEvent event) {
         blockCreativePeripheral = new BlockCreativePeripheral();
         GameRegistry.registerBlock(blockCreativePeripheral, BlockCreativePeripheral.NAME);
     }
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event){
+    public void init(FMLInitializationEvent event) {
         GameRegistry.registerTileEntity(TileEntityCreativePeripheral.class, TileEntityCreativePeripheral.NAME);
         ComputerCraftAPI.registerPeripheralProvider(new TileEntityCreativePeripheral.Provider());
         ComputerCraftAPI.registerTurtleUpgrade(new TurtleCreativePeripheral());
--- src/main/java/io/github/viniciuslrangel/CreativePeripheral/TileEntityCreativePeripheral.java
+++ src/main/java/io/github/viniciuslrangel/CreativePeripheral/TileEntityCreativePeripheral.java
@@ -1,5 +1,6 @@
 package io.github.viniciuslrangel.CreativePeripheral;
 
+import dan200.computercraft.api.filesystem.IMount;
 import dan200.computercraft.api.lua.ILuaContext;
 import dan200.computercraft.api.lua.LuaException;
 import dan200.computercraft.api.peripheral.IComputerAccess;
@@ -13,7 +14,16 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * By viniciuslrangel
@@ -21,11 +31,10 @@ import java.util.HashMap;
 public class TileEntityCreativePeripheral extends TileEntity implements IPeripheral {
 
     public static final String NAME = "tileEntityCreativePeripheral";
-    public static final String PERIPHERAL = "creativePeripheral";
+    public static final String PERIPHERAL = "Creative";
 
-    private static final String[] methods = {};
-
-    public TileEntityCreativePeripheral(){}
+    private static final String[] methods = {"getPlayers", "getWorlds", "getWorldSeed", "getWorldTime", "isWorldBlockLoaded", "getBlock", "setBlock"};
+    private static Mount mount = new Mount();
 
     @Override
     public String getType() {
@@ -34,55 +43,170 @@ public class TileEntityCreativePeripheral extends TileEntity implements IPeriphe
 
     @Override
     public String[] getMethodNames() {
-        return new String[]{"getWorlds", "getPlayers", ""};
+        return methods;
     }
 
     @Override
     public Object[] callMethod(IComputerAccess computer, ILuaContext context, int method, Object[] arguments) throws LuaException, InterruptedException {
         WorldServer[] worlds = MinecraftServer.getServer().worldServers;
-        switch (method){
+        switch (method) {
             case 0:
-                worlds = MinecraftServer.getServer().worldServers;
-                HashMap<Integer, Integer> toReturn = new HashMap<>();
-                for(WorldServer world:worlds)
-                    toReturn.put(toReturn.size()+1, world.provider.getDimensionId());
-                return new Object[]{toReturn};
-            case 1:
-                if(arguments.length > 0 && !(arguments[0] instanceof Boolean))
-                    throw new LuaException("getPlayers([boolean uuid])");
-                HashMap<Integer, Object> players = new HashMap<>();
-                for(WorldServer world:worlds)
-                    for(Object player:world.playerEntities)
-                        if(arguments.length > 0  && ((Boolean) arguments[0]) == true)
-                            players.put(players.size()+1, ((EntityPlayer) player).getUniqueID().toString());
+                if (arguments.length > 1 && !(arguments[0] instanceof Boolean))
+                    throw new LuaException("Usage: getPlayers([boolean uuid])");
+                HashMap<Integer, String> players = new HashMap<>();
+                for (WorldServer world : worlds)
+                    for (Object player : world.playerEntities)
+                        if (arguments.length > 0 && ((Boolean) arguments[0]))
+                            players.put(players.size() + 1, ((EntityPlayer) player).getUniqueID().toString());
                         else
-                            players.put(players.size()+1, ((EntityPlayer) player).getName());
+                            players.put(players.size() + 1, ((EntityPlayer) player).getName());
                 return new Object[]{players};
+            case 1:
+                HashMap<String, Integer> toReturn = new HashMap<>();
+                for (WorldServer world : worlds)
+                    toReturn.put(world.provider.getDimensionName(), world.provider.getDimensionId());
+                return new Object[]{toReturn};
+            case 2:
+                if (arguments.length > 0 && !(arguments[0] instanceof Double)) {
+                    throw new LuaException("Usage: getWorldSeed(number dimensionId)");
+                }
+                int id;
+                if (arguments.length > 0)
+                    id = ((Double) arguments[0]).intValue();
+                else
+                    id = 0;
+                for (WorldServer world : worlds)
+                    if (world.provider.getDimensionId() == id)
+                        return new Object[]{world.provider.getSeed()};
+            case 3:
+                if (arguments.length < 1 || !(arguments[0] instanceof Double)) {
+                    throw new LuaException("Usage: getWorldTime(number dimensionId)");
+                }
+                id = ((Double) arguments[0]).intValue();
+                for (WorldServer world : worlds)
+                    if (world.provider.getDimensionId() == id)
+                        return new Object[]{world.provider.getWorldTime()};
+            case 4:
+                if (arguments.length < 4)
+                    throw new LuaException("Usage: isWorldBlockLoaded(number dimensionId, number x, number y, number z)");
+                for (int i = 0; i < 4; i++)
+                    if (!(arguments[i] instanceof Double))
+                        throw new LuaException("Usage: isWorldBlockLoaded(number dimensionId, number x, number y, number z)");
+                id = ((Double) arguments[0]).intValue();
+                int x = ((Double) arguments[1]).intValue();
+                int y = ((Double) arguments[2]).intValue();
+                int z = ((Double) arguments[3]).intValue();
+                for (WorldServer world : worlds)
+                    if (world.provider.getDimensionId() == id)
+                        return new Object[]{world.isBlockLoaded(new BlockPos(x, y, z))};
+            case 5:
+                if (arguments.length < 4)
+                    throw new LuaException("Usage: getBlock(number dimensionId, number x, number y, number z)");
+                for (int i = 0; i < 4; i++)
+                    if (!(arguments[i] instanceof Double))
+                        throw new LuaException("Usage: getBlock(number dimensionId, number x, number y, number z)");
+                id = ((Double) arguments[0]).intValue();
+                x = ((Double) arguments[1]).intValue();
+                y = ((Double) arguments[2]).intValue();
+                z = ((Double) arguments[3]).intValue();
+                pos = new BlockPos(x, y, z);
+                for (WorldServer world : worlds)
+                    if (world.provider.getDimensionId() == id) {
+                        TileEntity te = world.getTileEntity(pos);
+                        return new Object[]{
+                                te.getBlockType().getUnlocalizedName(),
+                                te.getBlockMetadata(),
+                                te.getTileData().toString()
+                        };
+                    }
         }
+
         return null;
     }
 
-    @Override
-    public void attach(IComputerAccess computer) {}
-
-    @Override
-    public void detach(IComputerAccess computer) {}
-
-    @Override
-    public boolean equals(IPeripheral other) {
-        return other == null ? this == null : other.equals(this);
+    private EntityPlayer getPlayer(String nick) {
+        for (WorldServer world : MinecraftServer.getServer().worldServers)
+            for (Object player : world.playerEntities)
+                if (((EntityPlayer) player).getName().equalsIgnoreCase(nick))
+                    return (EntityPlayer) player;
+        return null;
     }
 
 
-    public static class Provider implements IPeripheralProvider{
+    @Override
+    public void attach(IComputerAccess computer) {
+        computer.mount(PERIPHERAL, mount);
+    }
+
+    @Override
+    public void detach(IComputerAccess computer) {
+        computer.unmount(PERIPHERAL);
+    }
+
+    @Override
+    public boolean equals(IPeripheral other) {
+        return other != null && other == this;
+    }
+
+
+    public static class Provider implements IPeripheralProvider {
 
         @Override
         public IPeripheral getPeripheral(World world, BlockPos pos, EnumFacing side) {
             TileEntity te = world.getTileEntity(pos);
-            if(te instanceof TileEntityCreativePeripheral){
+            if (te instanceof TileEntityCreativePeripheral) {
                 return (IPeripheral) te;
             }
             return null;
+        }
+    }
+
+    public static class Mount implements IMount {
+
+        private static final String PATH = "/assets/creativeperipheral/lua/";
+        private List<String> files;
+
+        public Mount() {
+            files = new ArrayList<>();
+            try {
+                Path main = Paths.get(getClass().getResource(PATH).toURI());
+
+                try (DirectoryStream<Path> directory = Files.newDirectoryStream(main)) {
+                    for (Path file : directory) {
+                        files.add(file.getFileName().toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public boolean exists(String path) throws IOException {
+            return path.isEmpty() || files.contains(path);
+        }
+
+        @Override
+        public boolean isDirectory(String path) throws IOException {
+            return path.isEmpty();
+        }
+
+        @Override
+        public void list(String path, List<String> contents) throws IOException {
+            contents.addAll(files);
+        }
+
+        @Override
+        public long getSize(String path) throws IOException {
+            return 0;
+        }
+
+        @Override
+        public InputStream openForRead(String path) throws IOException {
+            if (!files.contains(path)) throw new IOException();
+            return getClass().getResourceAsStream(PATH + path);
         }
     }
 
